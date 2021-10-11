09e26b0f84321677e2fb7bad24cbc7898a6d77cc
éde <edemarinho@outlook.com>
2021-10-11 18:05:56 -0300

    Implements the landing page behaviour and content

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Menu } from "semantic-ui-react"
 function NavBar () {
 
     return(
-        <Menu fixed='top' inverted>
+        <Menu size='massive' fixed='top' inverted>
             <Menu.Item name='home'/>
             <Menu.Item name='database'/>
             <Menu.Item name='about'/>
